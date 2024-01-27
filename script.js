0f8434f49b26eda9c9057d08e02ddabb4fa2c3d4
santiago quiceno <114130306+SantiCoco11@users.noreply.github.com>
2024-01-27 10:21:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const whatsappBtn = document.getElementById("whatsapp-btn");
+
+whatsappBtn.addEventListener("click", () => {
+    window.open("https://wa.me/+573136556427");
+});
+
+const room1Btn = document.getElementById("room1-btn");
+const room2Btn = document.getElementById("room2-btn");
+const room3Btn = document.getElementById("room3-btn");
+const room4Btn = document.getElementById("room4-btn");
+
+room1Btn.addEventListener("click", () => {
+    window.open("https://wa.me/+573136556427");
+});
+
+room2Btn.addEventListener("click", () => {
+    window.open("https://wa.me/+573136556427");
+});
+
+room3Btn.addEventListener("click", () => {
+    window.open("https://wa.me/+573136556427");
+});
+
+room4Btn.addEventListener("click", () => {
+    window.open("https://wa.me/+573136556427");
+});
+
